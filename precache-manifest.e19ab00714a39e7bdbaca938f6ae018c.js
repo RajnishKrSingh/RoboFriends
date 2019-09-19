6002ce26b00e97f0fcd5fe238a7956bc14845e01
Rajnish Kr Singh <rajnishsinghrs344@gmail.com>
2019-09-19 20:40:38 +0530

    Updates

---
+++ precache-manifest.e19ab00714a39e7bdbaca938f6ae018c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50a614cf3c8b57f82869b09bc41bf878",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "28db74d851e611b7b374",
+    "url": "/RoboFriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "da12a2ef7fb2ff161fe5",
+    "url": "/RoboFriends/static/css/main.baf2e6f9.chunk.css"
+  },
+  {
+    "revision": "28db74d851e611b7b374",
+    "url": "/RoboFriends/static/js/2.71af9bae.chunk.js"
+  },
+  {
+    "revision": "da12a2ef7fb2ff161fe5",
+    "url": "/RoboFriends/static/js/main.23408176.chunk.js"
+  },
+  {
+    "revision": "a55e004cb0cc29c5933a",
+    "url": "/RoboFriends/static/js/runtime~main.5e3a09ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
